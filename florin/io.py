8813ccc14b9c5603da0adb977b762946d969883b
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-10-12 16:36:37 -0400

    added save functions

--- florin/io.py
+++ florin/io.py
@@ -4,7 +4,7 @@ import os
 
 import h5py
 import numpy as np
-from scipy.ndimage import imread
+from scipy.misc import imread, imsave
 
 
 logging.basicConfig(
@@ -13,8 +13,33 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def load(path):
-    pass
+def load(path, **kws):
+    """Load an image or image volume.
+
+    Parameters
+    ----------
+    path : str
+        Path to the image(s).
+    key : str, optional
+        Key for an HDF5 file.
+
+    Returns
+    -------
+    vol : array-like
+        The image volume.
+    """
+    if os.path.isdir(path) or path.rfind('*') >= 0:
+        vol = load_images(path)
+    else:
+        _, ext = os.path.splitext(path)
+        if ext == '.h5':
+            vol = load_hdf5(path, key=kws['key'] if 'key' in kws else None)
+        elif ext == '.npy':
+            vol = load_npy(path)
+        else:
+            vol = load_image(path)
+
+    return vol
 
 
 def load_image(path):
@@ -158,17 +183,103 @@ def load_hdf5(path, key=None):
     return img
 
 
-def save(img, path, format='images', **kws):
-    pass
+def save(img, path, **kws):
+    """Save an image or volume to file.
+
+    Parameters
+    ----------
+    img : array-like
+        The image or volume to save.
+    path : str
+        The path to save the volume to.
+    format : str, optional
+        The file format to save as. Default: 'png'
+    key : str, optional
+        Name of the HDF5 dataset to save the volume to if using HDF5.
+
+    Notes
+    -----
+    When passing ``format`` as an argument, passing 'h5' or 'npy' will save to
+    an HDF5 and a .npy file, respectively. Valid image formats can be found
+    `here <http://pillow.readthedocs.io/en/3.4.x/handbook/image-file-formats.html>`_
+    """
+    fmt = kws['format'] if 'format' in kws else 'png'
+
+    if os.path.isdir(path):
+        save_images(vol, path, format=fmt)
+    else:
+        _, ext = os.path.splitext(path)
+        if ext == '.h5' or fmt == 'h5':
+            save_hdf5(vol, path, key=kws['key'] if 'key' in kws else 'stack')
+        elif ext == '.npy' or fmt == 'npy':
+            save_npy(vol, path)
+        else:
+            save_images(vol, path, format=fmt)
 
 
-def save_images(vol):
-    pass
+def save_images(vol, path, fmt='png'):
+    """Save one or more images from an image volume.
+
+    Parameters
+    ----------
+    vol : array-like
+        The image/volume to save as images.
+    path : str
+        The directory to save the images to.
+    """
+    logger.info('Saving images to {}'.format())
+
+    try:
+        if vol.ndim == 2:
+            imsave(path, vol, format=fmt)
+        else:
+            for i in range(vol.shape[0]):
+                imsave(path, vol[1])
+    except (IOError, OSError) as e:
+        logger.error('Could not write images to {}'.format(path))
+        logger.error(str(e))
 
 
-def save_hdf5(vol, path, key):
-    pass
+def save_hdf5(vol, path, key='stack'):
+    """Save image data to an HDF5 file.
+
+    Parameters
+    ----------
+    vol : array-like
+        The image/volume to save.
+    path : str
+        Path to save the volume to.
+    key : str, optional
+        Name of the dataset to save the volume to. Default: 'stack'.
+    """
+    logger.info('Saving volume to {}'.format(path))
+
+    try:
+        with h5py.File(path, 'r+') as f:
+            if key in f:
+                del f[key]
+            f.create_dataset(key, vol)
+    except (IOError, OSError) as e:
+        logger.error('Could not write volume to {}'.format(path))
+        logger.error(str(e))
 
 
 def save_npy(vol, path):
-    pass
+    """Save image data to a .npy file.
+
+    Parameters
+    ----------
+    vol : array-like
+        The image/volume to save.
+    path : str
+        Path to save the volume to.
+    key : str, optional
+        Name of the dataset to save the volume to. Default: 'stack'.
+    """
+    logger.info('Saving volume to {}'.format(path))
+
+    try:
+        np.save(path, vol)
+    except (IOError, OSError) as e:
+        logger.error('Could not write volume to {}'.format(path))
+        logger.error(str(e))
