5f685d148eb80558451752087cf5acd0796d2cd9
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-10-13 15:12:59 -0400

    add initial thresholding actions

---
+++ florin/actions/thresholding.py
@@ -0,0 +1,24 @@
+"""Composable actions for image thresholding and binarization.
+
+Classes
+-------
+ThresholdingAction
+LocalAdaptiveThresholding
+"""
+from florin.thresholding import local_adaptive_thresholding
+from florin.actions.base import BaseAction
+
+
+class ThresholdingAction(BaseAction):
+    def __init__(self, name=None, next=None, shape=None, threshold=0.25):
+        super(ThresholdingAction, self).__init__(name=name, next=next)
+        self.shape = shape
+        self.threshold = threshold
+
+
+class LocalAdaptiveThresholding(ThresholdingAction):
+    def __call__(self, img):
+        return local_adaptive_thresholding(
+            img,
+            shape=self.shape,
+            threshold=self.threshold)
