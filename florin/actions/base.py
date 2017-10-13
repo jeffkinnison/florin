a66b1b3fa7b89401e2564e03fbe8600ddffc16f4
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-10-13 13:17:07 -0400

    add base class for composable actions

---
+++ florin/actions/base.py
@@ -0,0 +1,41 @@
+"""Base class for FLoRiN image processing, filtering, and output actions.
+
+Classes
+-------
+BaseAction
+InvalidActionError
+"""
+
+class InvalidActionError(ValueError):
+    def __init__(self, value):
+        msg = 'Invalid action: {}'.format(value)
+        super(InvalidActionError, self).__init__(msg)
+
+class BaseAction(object):
+    """Base class for FLoRiN image processing, filtering, and output actions.
+
+    Parameters
+    ----------
+
+    """
+    def __init__(self, name=None, next=None):
+        self.name = name if name is not None else 'base'
+        self.next = next
+
+    def __call__(self):
+        raise NotImplementedError
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, value):
+        if isinstance(value, BaseAction):
+            self.__next = value
+        else:
+            raise InvalidActionError(value)
+
+    @next.deleter
+    def next(self):
+        del self.__next
