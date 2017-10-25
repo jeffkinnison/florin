abc94819178e5a5e5d7f37d6c1c9427727f13d4b
Jeff Kinnison <jeffdkinnison@gmail.com>
2017-10-25 12:22:13 -0400

    add initial stages code

--- florin/stages.py
+++ florin/stages.py
@@ -5,52 +5,71 @@ Classes
 BaseStage
     Base class for all stages.
 PreprocessingStage
+    Stage for performing preliminary grayscale adjustments, tiling, etc.
 SegmentationStage
+    Stage for thresholding and binary morphological operations.
 FilteringStage
+    Stage for connected components detection and filtering into classes.
 OutputStage
+    Stage for outputting results to file.
 """
+from florin.actions import BaseAction, ThresholdingAction,\
+                           LocalAdaptiveThresholding
+
 
 class BaseStage(object):
-    def __init__(self):
-        pass
+    def __init__(self, actions=None):
+        self.actions = []
 
-    def __call__(self):
-        pass
+        if actions is not None:
+            map(self.push_action, actions)
 
-    def push_action(self):
-        pass
+    def __call__(self, img):
+        for action in actions:
+            img = action(img)
+        return img
 
-    def pop_action(self):
-        pass
+    def push_action(self, action):
+        if not isinstance(action, BaseAction):
+            print('Ya done goofed!')
+        self.actions.append(action)
+
+    def pop_action(self, action):
+        try:
+            action = self.actions.pop()
+        except IndexError:
+            action = None
+        return action
 
 
 class PreprocessingStage(BaseStage):
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        pass
+    def __init__(self, actions=None):
+        super(PreprocessingStage, self).__init__(actions=actions)
 
 
 class SegmentationStage(BaseStage):
-    def __init__(self):
-        pass
+    def __init__(self, shape=None, threshold=0.25, actions=None):
+        if actions is None:
+            actions = []
 
-    def __call__(self):
-        pass
+        thresh = False
+        for action in actions:
+            if isinstance(action, ThresholdingAction):
+                thresh = True
+
+        if not thresh:
+            thresh = LocalAdaptiveThresholding(shape=shape,
+                                               threshold=threshold)
+            actions = [thresh] + actions
+
+        super(SegmentationStage, self).__init__(actions=actions)
 
 
 class FilteringStage(BaseStage):
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        pass
+    def __init__(self, actions=None):
+        super(FilteringStage, self).__init__(actions=actions)
 
 
 class OutputStage(BaseStage):
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        pass
+    def __init__(self, actions=None):
+        super(OutputStage, self).__init__(actions=actions)
