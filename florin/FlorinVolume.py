bcc433ae63462a328f3ba1e029725e480955342f
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-16 10:41:01 -0500

    add map() function, rework initializer

--- florin/FlorinVolume.py
+++ florin/FlorinVolume.py
@@ -5,64 +5,66 @@ from skimage.morphology import remove_small_objects
 
 from utils import threshold_bradley_nd, load_volume, save_imgs
 
-from florin.io import *
-from florin.tiling import tile_3d
+from florin.compose import compose
+from florin.io import load, save
+from florin.tiling import tile
 
-import florin
 
-class FlorinVolume:
-    def __init__ (self, data = None, shape = None, address = (0,0,0)):
+class FlorinVolume(object):
+    """Container for data loaded into FLoRIN.
 
-        if data is not None:
-            self.data = data
-            if 'image' in self.keys():
-                self.shape = self['image'].shape
-            elif 'threshold' in self.keys():
-                self.shape = self['image'].shape
-            else: self.shape = None
-        else:
-            if shape is not None:
-                self.data = {'image': np.zeros(shape)}
-                self.shape = shape
-            else:
-                self.data = dict()
-                self.shape = None
+    Parameters
+    ----------
+    data : dict, optional
+        The image data to processs.
+    address : tuple of int, optional
+        The index of the origin of this data within a larger volume.
 
-        self.tile_gen = (i for i in [self])
+    Attributes
+    ----------
+    data : array_like
+        The image data to process.
+    address : tuple of int
+        The address of the origin of the array. When tiling, this allows the
+        tiles to be stitched together.
+    tiler : callable
+        Function or generator to split ``data`` into sub-arrays.
+    operations : list of callable
+        The operations to run on this data.
+
+    """
+
+    def __init__ (self, data, operations=None, address=(0,0,0), tiler=None):
+        self.data = data
+        self.tiler = tiler if tiler is not None else tile(self.data)
         self.address = address
-        
+        self.function_chain = []
 
-    def load (self, path):
+
+    def load(self, path):
         self['image'] = load(path)
         self.shape = self['image'].shape
 
     def save (self, path):
         pass
 
-    def add (self, func):
-        #self.tile_gen = (func(tile) for tile in self.tile_gen)
-        return func(self)
+    def add(self, func):
+        self.function_chain.append(func)
 
-    def join (self):
-        vol = florin.FlorinVolume.FlorinVolume(shape = self.shape)
-        for tile in self.tile_gen:
-            for k in tile.keys():
-                if k not in vol.keys(): vol[k] = np.zeros(self.shape)
-                vol[k][tile.address[0]:tile.address[0]+tile.shape[0],\
-                       tile.address[1]:tile.address[1]+tile.shape[1],\
-                       tile.address[2]:tile.address[2]+tile.shape[2]] += tile[k]
-        return vol
+    def map(self):
+        self.result = next(map(compose(*self.function_chain), [self.data]))
 
-    def __getitem__ (self, key):
-        if key not in self.data.keys():
+
+    def __getitem__(self, key):
+        if key not in self.data:
             if key == 'threshold':
                 raise KeyError("'threshold' not in tile. Have you thresholded your data?")
             if key == 'image':
                 raise KeyError("'image' not in tile. The tile is empty.")
         return self.data[key]
 
-    def __setitem__ (self, key, value):
+    def __setitem__(self, key, value):
         self.data[key] = value
 
-    def keys (self):
+    def keys(self):
         return self.data.keys()
