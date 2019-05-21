2d669b714a37d174a80196b727cadbceac4cf0ec
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-21 09:50:32 -0500

    remove version 2 support until verification and update dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/jeffkinnison/florin',
     author='Jeff Kinnison, Elia Shahbazi',
     author_email='jkinniso@nd.edu, ashahbaz@nd.edu',
-    packages=['florin',],
+    packages=['florin', 'florin.pipelines'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -15,8 +15,6 @@ setup(
         'License :: MIT',
         'Topic :: Computer Vision :: Image Segmentation',
         'Topic :: Computer Vision :: Image Processing Pipeline',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -27,9 +25,11 @@ setup(
     ],
     keywords='machine_learning hyperparameters distributed_computing',
     install_requires=[
-        'scipy>=0.19.0',
-        'numpy>=1.12.0',
-        'scikit-image>=0.13.1',
-        'h5py'
+        'h5py',
+        'mpi4py',
+        'numpy',
+        'pathos',
+        'scikit-image',
+        'scipy',
     ]
 )
