599697bfb64bda3eb1e971d135890f700c930721
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-21 14:10:23 -0500

    helper imports

--- florin/__init__.py
+++ florin/__init__.py
@@ -1,2 +1,8 @@
 from .closure import florinate
 from .io import load, save
+from florin.pipelines import BalsamPipeline as Balsam
+from florin.pipelines import MPIPipeline as MPI
+from florin.pipelines import MultiprocessingPipeline as Multiprocess
+from florin.pipelines import MultithreadingPipeline as Multithread
+from florin.pipelines import SerialPipeline as Serial
+from florin.pipelines import WorkQueuePipeline as WorkQueue
