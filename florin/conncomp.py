08222dc353afb483ff0b4ee863292d1c46f33941
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-22 16:42:56 -0500

    update regionprops objects with useful data

--- florin/conncomp.py
+++ florin/conncomp.py
@@ -8,11 +8,22 @@ regionprops
     Compute various properties of labeled connected components.
 """
 
+import numpy as np
 import skimage.measure
 
 from florin.closure import florinate
 
+@florinate
+def regionprops(image, **kwargs):
+    objs = skimage.measure.regionprops(image, **kwargs)
+
+    for obj in objs:
+        obj.original_image_shape = image.shape
+        if image.ndim == 2:
+            obj.height, obj.width = np.asarray(obj.bbox[2:]) - np.asarray(obj.bbox[:2])
+        else:
+            obj.depth, obj.height, obj.width = np.asarray(obj.bbox[3:]) - np.asarray(obj.bbox[:3])
+
+    return objs
 
 label = florinate(skimage.measure.label)
-
-regionprops = florinate(skimage.measure.regionprops)
