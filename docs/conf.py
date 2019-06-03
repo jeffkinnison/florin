d46c0e1d0b3865f7708719afecd20dd7e3372867
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-06-03 13:13:59 +0300

    prep for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -10,13 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-from unittest.mock import MagicMock
-sys.path.insert(0, os.path.abspath('..'))
-
-import florin
-import florin.pipelines
 
 class Mock(MagicMock):
     @classmethod
@@ -25,6 +18,14 @@ class Mock(MagicMock):
 
 MOCK_MODULES = ['mpi4py']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+import os
+import sys
+from unittest.mock import MagicMock
+sys.path.insert(0, os.path.abspath('..'))
+
+import florin
+import florin.pipelines
 
 # -- Project information -----------------------------------------------------
 
