0522a952cf47a212f2cc3f81a8caeea698b32c90
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-06-03 14:27:08 +0300

    fixes to setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name='florin',
-    version='0.1a1',
+    version='0.0.1',
     description='Fast image segmentation without needing to learn a thing.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     url='https://github.com/jeffkinnison/florin',
     author='Jeff Kinnison, Elia Shahbazi',
     author_email='jkinniso@nd.edu, ashahbaz@nd.edu',
@@ -11,21 +17,22 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: MIT',
-        'Topic :: Computer Vision :: Image Segmentation',
-        'Topic :: Computer Vision :: Image Processing Pipeline',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Operating System :: MacOS',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: System :: Distributed Computing',
     ],
     keywords='machine_learning hyperparameters distributed_computing',
     install_requires=[
         'h5py',
-        'mpi4py',
+        'mpi4py>=3.0.0',
         'numpy',
         'pathos',
         'scikit-image',
