f4d7a0bb75b8ae81ec8e5ada2b05803f69d388f9
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-06-03 13:32:51 +0300

    prep for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -16,13 +16,13 @@ import sys
 from unittest.mock import MagicMock
 sys.path.insert(0, os.path.abspath('..'))
 
-# class Mock(MagicMock):
-#     @classmethod
-#     def __getattr__(cls, name):
-#         return MagicMock()
-#
-# MOCK_MODULES = ['mpi4py']
-# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mpi4py', mpi4py.futures]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 autodoc_mock_imports = ['mpi4py', 'mpi4py.futures']
 
