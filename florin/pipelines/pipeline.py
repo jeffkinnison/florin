f8ae142e03565010d6c71713f2d96e2a11b18737
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-06-20 14:34:59 -0500

    update Pipeline to work with graphs

--- florin/pipelines/pipeline.py
+++ florin/pipelines/pipeline.py
@@ -6,14 +6,17 @@ Pipeline
     Base pipeline.
 """
 
-import collections
+from collections import Sequence
 import functools
 import inspect
 import re
 
 import dill
+import networkx as nx
 
+from florin.closure import florinate
 from florin.compose import compose
+from florin.graph import FlorinOrderedMultiDiGraph, FlorinNode
 from florin.tiling import join
 
 
@@ -25,82 +28,61 @@ class Pipeline(object):
     operations : callables
         The operations/functions/callable classes to run in this pipeline.
 
+    Attributes
+    ----------
+    operations : florin.graph.FlorinOrderedDiGraph
+        The operations in the pipeline converted into a directed graph, with
+        operations stored as nodes and data flow/dependencies stored as edges.
     """
 
     def __init__(self, *operations):
-        self.operations = []
+        self.operations = FlorinOrderedMultiDiGraph()
 
-        for operation in operations:
-            self.add(operation)
+        tiled = False
+        subpipe = False
+
+        # Add operations to the execution graph.
+        for i, operation in enumerate(operations):
+            # Wrap any Pipeline instances in FlorinNodes before inserting into
+            # the graph
+            if isinstance(operation, Pipeline):
+                operation = FlorinNode(operation)
+
+            # If tiling and a subpipeline were supplied without an ensuing
+            # join, add a join in after the pipeline.
+            # Otherwise, if only a subpipeline was encountered with no prior
+            # tile, make sure to add the wrapped version as a dependency.
+            if tiled and subpipe and not re.search(r'join', operation.__name__):
+                join_op = join(subpipe)
+                self.operations.add(join_op)
+                operation.args = (join_op,) + operation.args
+                tiled = False
+                subpipe = False
+            elif subpipe:
+                operation.args = (subpipe,) + operation.args
+                subpipe = False
+
+            # As a convenience, if the current operation is not the first and
+            # has no dependencies, add the previous operation as a dependency.
+            depcount = sum([1 if isinstance(dep, FlorinNode) else 0
+                            for dep in operation.args])
+            if i > 0 and depcount == 0:
+                operation.args = (operations[i - 1],) + operation.args
+
+            # Insert the operation into the graph.
+            self.operations.add(operation)
+
+            # Bookeeping for finding subpipelines and tiling operations.
+            if re.search(r'Pipeline', operation.__name__):
+                subpipe = operation
+            elif re.search(r'tile', operation.__name__):
+                tiled = True
 
     def __call__(self, data):
-        for i, operation in enumerate(self.operations):
-            if not isinstance(operation, Pipeline) and re.search(r'tile_generator', operation.__name__):
-                if len(self.operations) > i + 1 and not isinstance(self.operations[i + 1], Pipeline):
-                    start = i + 1
-                    end = start
-                    for j, subop in enumerate(self.operations[start:]):
-                        if re.search(r'join_tiles|save', subop.__name__):
-                            break
-                        end += 1
-
-                    if end == len(self.operations):
-                        self.operations.append(join())
-
-                    subops = self.operations[start:end]
-                    self.operations[start] = self.__class__(*subops)
-                    for j in range(start + 1, end):
-                        self.operations.pop(j)
-            if isinstance(operation, Pipeline):
-                try:
-                    if not re.search(r'reconstruct', self.operations[i + 1].__name__):
-                        self.operations.insert(i + 1, join())
-                except IndexError:
-                    self.operations.append(join())
-
-        self.operations = compose(*self.operations)
-        if isinstance(data, (str, tuple)) or not inspect.isgenerator(data) and not isinstance(data, collections.Sequence):
+        # Wrap data to be an iterable if it is not one already.
+        if not isinstance(data, Sequence) and not inspect.isgenerator(data) or isinstance(data, str):
             data = [data]
         return self.run(data)
-
-    def __contains__(self, func):
-        return func in self.operations \
-            or any([o.__name__ == func.__name__ for o in self.operations])
-
-    def __getitem__(self, start, stop=None, step=1):
-        return self.operations[start] if stop is None \
-            else self.operations[slice(start, stop, step)]
-
-    def __setitem__(self, idx, func):
-        self.operations[idx] = func
-
-    def __len__(self):
-        return len(self.operations)
-
-    def add(self, func):
-        """Append a callable to this pipeline.
-
-        Parameters
-        ----------
-        func : callable
-            New function to add.
-        """
-        self.operations.append(func)
-
-    def dump(self, path):
-        """Save this pipeline to file.
-
-        Parameters
-        ----------
-        path : str
-            Path to the file to write this pipeline to.
-        """
-        with open(path, 'w') as f:
-            dill.dump(self, path)
-
-    def dumps(self):
-        """Serialize this pipeline as a string."""
-        return dill.dumps(self)
 
     def run(self, data):
         """Run data through the pipeline.
@@ -108,11 +90,17 @@ class Pipeline(object):
         Parameters
         ----------
         data
-            Input to the first function in the pipeline.
+            The input to the first function in the pipeline.
 
         Returns
         -------
         result
-            The result of applying the pipeline to ``data``.
+            The final output of applying the pipeline to ``data``.
+
+        Notes
+        -----
+        To implement new kinds of execution models, this function should be
+        overridden. See other subclasses in the ``florin.pipelines`` module for
+        examples of how to override ``run()``.
         """
         raise NotImplementedError
